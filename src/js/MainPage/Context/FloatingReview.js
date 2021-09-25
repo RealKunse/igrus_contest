1ab9d6e4ef53e08ff57c04918511ab4e60bdb395
RealKunse <gml4009@gmail.com>
2021-09-25 15:17:27 +0900

    Main Context Review

--- src/js/MainPage/Context/FloatingReview.js
+++ src/js/MainPage/Context/FloatingReview.js
@@ -6,13 +6,52 @@ import $ from 'jquery';
 class FloatingReview extends React.Component {
     componentDidMount() {
         $(window).scroll((e) => {
-            $('#review').css('margin-top',window.scrollY /15  + 'px')
+                $('#review').css('margin-top', window.scrollY / 15 + 'px')
             }
         )
     }
 
-    render(){
-        return(
+    constructor(props) {
+        super(props);
+        this.state = {
+            curReview: 1,
+        }
+    }
+
+    moveToLeftImage = () => {
+        if(this.state.curReview !== 1) {
+            this.setState({curReview: this.state.curReview - 1});
+        } else {
+            this.setState({curReview : 6});
+        }
+        this.useAnimation()
+    };
+
+    moveToRightImage = () => {
+        if(this.state.curReview !== 6){
+            this.setState({curReview: this.state.curReview + 1});
+        } else {
+            this.setState({curReview : 1});
+        }
+        this.useAnimation()
+    };
+
+    useAnimation = () => {
+        document.getElementsByClassName('bx-slider')[0].style.transform = `translate3d(-${this.state.curReview * 280}px,0,0)`
+        document.getElementsByClassName('bx-slider')[0].style.transitionDuration = '0.5s'
+    }
+
+    disableStyleAnimation = () => {
+
+    };
+
+    activateStyleAnimaation = () => {
+
+    }
+
+
+    render() {
+        return (
             <div id={"review"}>
                 <div className={'bx-wrapper'}>
                     <div className={'bx-viewport'}>
@@ -43,8 +82,18 @@ class FloatingReview extends React.Component {
                             </div>
 
 
-
                         </div>
+                    </div>
+                    <div className={'bx-control-box'}>
+                        <a className={'bx-prev'} onClick={this.moveToLeftImage}>
+                            <img src={'images/skin/review_prev.png'} alt={'prev'}/>
+                        </a>
+                        <span>
+                            {this.state.curReview} / 6
+                        </span>
+                        <a className={'bx-next'} onClick={this.moveToRightImage}>
+                            <img src={'images/skin/review_next.png'} alt={'next'}/>
+                        </a>
                     </div>
                 </div>
 
