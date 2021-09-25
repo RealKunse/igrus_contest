e63b001ea0675572e3dbb74c282fece86f80e2ab
RealKunse <gml4009@gmail.com>
2021-09-25 22:03:43 +0900

    Main Fixed

---
+++ src/js/Details/Context/Top.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class Top extends React.Component {
+    render() {
+        return (
+            <div >
+                <div className={'location'}>
+                    <div className={'path'}>
+                        <span>홈</span>
+                        <div className={'navi'}>
+                            <div className={'this'}>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Top;
--- src/js/Details/Details.js
+++ src/js/Details/Details.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import Top from "./Context/Top";
 
-class Details extends React.Component{
+class Details extends React.Component {
     render() {
         return (
             <div>
                 <Header/>
+                <div id={'container'} className={'goodsv_container'} style={{marginTop:100}}>
+                    <div id={'content'}>
+                        <div className={'goods-view'}>
+                            <Top/>
+                        </div>
+                    </div>
+                </div>
                 <Footer/>
             </div>
         );
